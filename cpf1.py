d00ed70a72bbbbb9a8ecf055ea64b919c55edfed
Tim <46388504+timlai4@users.noreply.github.com>
2019-03-20 17:31:19 -0400

    Add files via upload

--- cpf1.py
+++ cpf1.py
@@ -25,15 +25,39 @@ train_cpf1_df.columns = new_columns
 test_cpf1_df.columns = new_columns
  # Alternatively, https://stackoverflow.com/questions/34263772/how-to-generate-one-hot-encoding-for-dna-sequences
   #list(string) breaks the string into a list. 
-cols = [str(i) for i in range(20)]
+cols = [str(i) for i in range(34)]
 train_single_nucleotides = pd.DataFrame()
 test_single_nucleotides = pd.DataFrame()
-for i in range(20):
-  train_single_nucleotides[cols[i]] = [train_cpf1_df['20bp'][j][i] for j in range(15000)]
-  test_single_nucleotides[cols[i]] = [test_cpf1_df['20bp'][j][i] for j in range(1292)]
+train_dinucleotides = pd.DataFrame()
+test_dinucleotides = pd.DataFrame()
+for i in range(34):
+  train_single_nucleotides[cols[i]] = [train_cpf1_df['34bp'][j][i] for j in range(15000)]
+  test_single_nucleotides[cols[i]] = [test_cpf1_df['34bp'][j][i] for j in range(1292)]
 
-train_labels = pd.get_dummies(train_single_nucleotides)
-test_labels = pd.get_dummies(test_single_nucleotides)
+for i in range(32):
+  train_dinucleotides[cols[i] + cols[i]] = [train_cpf1_df['34bp'][j][i:i+2] for j in range(15000)]
+  test_dinucleotides[cols[i] + cols[i]] = [test_cpf1_df['34bp'][j][i:i+2] for j in range(1292)]
+
+train_single_encode = pd.get_dummies(train_single_nucleotides)
+test_single_encode = pd.get_dummies(test_single_nucleotides)
+
+train_di_encode = pd.get_dummies(train_dinucleotides)
+test_di_encode = pd.get_dummies(test_dinucleotides)
+
+
+train_labels= pd.concat([train_single_encode, train_di_encode], axis = 1)
+test_labels = pd.concat([test_single_encode, test_di_encode], axis = 1)
+
+# Free up some memory
+del train_single_nucleotides
+del test_single_nucleotides
+del train_dinucleotides
+del test_dinucleotides
+del train_single_encode
+del test_single_encode
+del train_di_encode
+del test_di_encode
+
 train_targets = train_cpf1_df['No background indel frequency']
 test_targets = test_cpf1_df['No background indel frequency']
 

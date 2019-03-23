e29651ea2bf2d6cad25239266625c6a5d680ea09
Tim <46388504+timlai4@users.noreply.github.com>
2019-03-22 23:11:59 -0400

    Added copyright info

--- cpf1_reg.py
+++ cpf1_reg.py
@@ -1,8 +1,22 @@
+# Copyright 2017 Google LLC
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # -*- coding: utf-8 -*-
 """
 Created on Wed Mar 20 23:54:17 2019
 
 @author: Tim
+I have edited this code originally created by Google. 
 """
 
 from __future__ import print_function
